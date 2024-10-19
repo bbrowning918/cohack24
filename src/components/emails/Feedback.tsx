cae97b102ca07f62c9586a797601782953a5a44b
benbrowning <ben@skillshark.com>
2024-10-19 12:30:46 -0600

    feedback email styling and layout

--- src/components/emails/Feedback.tsx
+++ src/components/emails/Feedback.tsx
@@ -34,53 +34,81 @@ export const Feedback = ({
         <div className="font-[family-name:var(--font-lato)]">
             <div className="flex flex-col justify-center w-full p-20">
                 <Image
+                    className="pb-8"
                     src={'/logos/jewel(black).svg'}
                     alt="Jewel logo"
                     width={132}
                     height={66}
                     priority
                 />
-                <div>Dear {firstName},</div>
-                <div>This summary covers your progress from {startDate} to {endDate}. Keep up the good work!</div>
-                <div>Before we dive into it, lets look over the goal you set for yourself:</div>
-                <ul className="list-disc">
-                    <li>Selected Goal: {selectedGoal}</li>
-                    <li>Importance: {goalImportance}</li>
+                <div className="pb-4">Dear {firstName},</div>
+                <div className="pb-4">This summary covers your progress from {startDate} to {endDate}. <br /> Keep up the good work!</div>
+                <div className="pb-4">Before we dive into it, lets look over the goal you set for yourself:</div>
+                <ul className="list-disc pb-4 ml-5">
+                    <li>
+                        <div className="flex">
+                            <div className="pr-2">Selected Goal:</div>
+                            <div>{selectedGoal}</div>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="flex">
+                            <div className="pr-2">Importance:</div>
+                            <div>{goalImportance}</div>
+                        </div>
+                    </li>
                 </ul>
                 <div className="text-2xl text-jewelSecondary pb-4">Key Insights</div>
 
                 <div className="text-jewelSecondary pb-2">Tasks Completed</div>
-                <div>{tasksCompleted}</div>
+                <div className="pb-4">{tasksCompleted}</div>
 
                 <div className="text-jewelSecondary pb-2">Challenges Faced</div>
-                <div>{challengesFaced}</div>
+                <div className="pb-4">{challengesFaced}</div>
 
                 <div className="text-jewelSecondary pb-2">Time Management Patterns</div>
-                <div>{timeManagement}</div>
+                <div className="pb-4">{timeManagement}</div>
 
                 <div className="text-jewelSecondary pb-2">Collaboration & Support</div>
-                <div>{collaboration}</div>
+                <div className="pb-4">{collaboration}</div>
 
                 <div className="text-jewelSecondary pb-2">Suggestions for Improvement</div>
-                <div>{suggestions}</div>
+                <div className="pb-4">{suggestions}</div>
 
                 <div className="text-jewelSecondary pb-2">Emotional Wellbeing</div>
-                <div>{emotionalWellbeing}</div>
+                <div className="pb-4">{emotionalWellbeing}</div>
 
                 <div className="text-jewelSecondary pb-2">Conclusion</div>
-                <div>{conclusion}</div>
+                <div className="pb-4">{conclusion}</div>
 
-                <div>What do you feel about this feedback?</div>
-                <span>&#x1F616; Terrible</span>
-                <span>&#x1F61F; Not So Good</span>
-                <span>&#x1F610; Indifferent</span>
-                <span>&#x1F604; Looks Good</span>
-                <span>&#x1F60D; Excellent</span>
+                <div className="font-bold text-center m-4">What do you feel about this feedback?</div>
+                <div className="flex justify-center gap-4 pb-4">
+                    <div className="text-center">
+                        <div>&#x1F616;</div>
+                        <div className="text-jewelSecondary font-semibold">Terrible</div>
+                    </div>
+                    <div className="text-center">
+                        <div>&#x1F61F;</div>
+                        <div className="text-jewelSecondary font-semibold">Not So Good</div>
+                    </div>
+                    <div className="text-center">
+                        <div>&#x1F610;</div>
+                        <div className="text-jewelSecondary font-semibold">Indifferent</div>
+                    </div>
+                    <div className="text-center">
+                        <div>&#x1F604;</div>
+                        <div className="text-jewelSecondary font-semibold">Looks Good</div>
+                    </div>
+                    <div className="text-center">
+                        <div>&#x1F60D;</div>
+                        <div className="text-jewelSecondary font-semibold">Excellent</div>
+                    </div>
+                </div>
 
-                <div>Is there anything you&#39;d like to add or modify in this summary? Your thoughts are valuable to
+                <div className="font-bold text-center m-4 pb-4">Is there anything you&#39;d like to add or modify in this summary?<br /> Your thoughts are valuable to
                     help refine future reflections!
                 </div>
-                <Button>Give us your Feedback!</Button>
+                <Button className="p-4">Give us your Feedback!</Button>
             </div>
             <div className="flex gap-6 flex-wrap items-center justify-between bg-jewelBlack p-20">
                 <Image
