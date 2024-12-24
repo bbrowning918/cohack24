f5921f4932e04259b38b18f82456cf8031860c69
kararAcode <63989704+kararAcode@users.noreply.github.com>
2024-12-24 01:21:53 -0600

    write signup route code in typescript

---
+++ src/pages/api/signup.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import db from "@/utils/supabaseUtil";
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+
+        try {
+            const { firstName, email, occupation, goal, emailFrequnecy } = req.body;
+            const addedProfile = await db.addProfile(firstName, email, occupation, goal, emailFrequnecy);
+            console.log("Added Profile: ", addedProfile);
+            res.status(200).json({ success: true, data: addedProfile });
+        }
+        catch (error) {
+            console.error("Error Adding Profile: ", error);
+            res.status(500).json({ success: false, error: "Error Adding Profile" });
+        }
+    }
+}
